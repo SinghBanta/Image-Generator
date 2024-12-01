66c1272712c0fe7fcc4186539bf15c2e97a02ed8
SinghBanta <singhbanta84@gmail.com>
2024-12-01 17:26:15 +0530

    add:"favicon"

--- script.js
+++ script.js
@@ -1,49 +1,16 @@
-//this code id just to check that api is working or not.
-// const key="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";
-// const inputText=document.getElementById("input");
-// const image=document.getElementById("image");
-// const GenBtn=document.getElementById("btn");
-
-// async function query(data) {
-// 	const response = await fetch(
-// 		"https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
-// 		{
-// 			headers: {
-// 				Authorization: `Bearer ${key}`,
-
-// 			},
-// 			method: "POST",
-// 			body: JSON.stringify({"inputs": inputText.value}),
-// 		}
-// 	);
-// 	const result = await response.blob();
-// 	return result;
-// }
-
-// async function generate(){
-
-// query().then((response) => {
-// 	const objectUrl=URL.createObjectURL(response);
-//     image.src=objectUrl;
-// });
-// }
-
-// GenBtn.addEventListener("click",()=>{
-//     generate();
-// });
-
 const key = "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";
 const inputText = document.getElementById("input");
 const image = document.getElementById("image");
 const GenBtn = document.getElementById("btn");
-const svg=document.getElementById("svg");
-const load=document.getElementById("loading");
-const ResetBtn=document.getElementById("reset");
-const downloadBtn=document.getElementById("download");
+const svg = document.getElementById("svg");
+const load = document.getElementById("loading");
+const ResetBtn = document.getElementById("reset");
+const downloadBtn = document.getElementById("download");
 
 async function query(data) {
   const response = await fetch(
     "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+
     {
       headers: {
         Authorization: `Bearer ${key}`,
@@ -52,45 +19,47 @@ async function query(data) {
       body: JSON.stringify({ inputs: inputText.value }),
     }
   );
+
   const result = await response.blob();
-  image.style.display="block"
-  load.style.display="none";
+  image.style.display = "block";
+  load.style.display = "none";
   return result;
 }
 
 async function generate() {
-    load.style.display="block";
+  load.style.display = "block";
   query().then((response) => {
     const objectUrl = URL.createObjectURL(response);
     image.src = objectUrl;
-    downloadBtn.addEventListener("click",()=>download(objectUrl));
+    downloadBtn.addEventListener("click", () => download(objectUrl));
   });
 }
 
 GenBtn.addEventListener("click", () => {
   generate();
-  svg.style.display="none";
+  svg.style.display = "none";
 });
 
-inputText.addEventListener("keydown",(e) => {
+inputText.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     generate();
-    svg.style.display="none";
+    svg.style.display = "none";
   }
 });
 
-ResetBtn.addEventListener("click",()=>{
-    inputText.value="";
-    window.location.reload();
+ResetBtn.addEventListener("click", () => {
+  inputText.value = "";
+  window.location.reload();
 });
 
-
-function download(objectUrl){
-
-    fetch(objectUrl).then(response=>response.blob()).then(file =>{
-        let a=document.createElement("a");
-        a.href=URL.createObjectURL(file);
-        a.download=new Date().getTime();
-        a.click();
-    }).catch(()=>alert("Failed to download"));
+function download(objectUrl) {
+  fetch(objectUrl)
+    .then((response) => response.blob())
+    .then((file) => {
+      let a = document.createElement("a");
+      a.href = URL.createObjectURL(file);
+      a.download = new Date().getTime();
+      a.click();
+    })
+    .catch(() => alert("Failed to download"));
 }
