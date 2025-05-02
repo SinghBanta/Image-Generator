8a3fc4f6e918463e0522cca8d445b022f81e67c3
Banta Singh <146963250+SinghBanta@users.noreply.github.com>
2025-05-02 16:40:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const downloadBtn = document.getElementById("download");
 
 async function query(data) {
   const response = await fetch(
-    "https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+    "YjUyOGVkYzA1NWE0NmI3ZDAzNjdhNDViNTg3NjhhYTg6NmM4ZDg3Y2Q5NWNkOTEyNzkwZDU0OTA3Mjg5ZjBjNWI=",
 
     {
       headers: {
